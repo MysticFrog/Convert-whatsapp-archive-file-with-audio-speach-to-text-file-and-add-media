755f1ad96db973cf6c2b735c52929d0e5df95e21
MysticFrog <andrew_jones0800@hotmail.com>
2024-07-15 14:55:47 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+#requirement. to enable Windows long path support
+#https://pip.pypa.io/warnings/enable-long-paths
+#do
+#python -m pip install --upgrade pip setuptools wheel
+#python -m pip install --upgrade emoji certifi cffi charset-normalizer ffmpeg-python filelock future huggingface-hub idna lxml more-itertools numpy nvidia-cublas-cu11 urllib3, typing-extensions, pyyaml, pycparser, packaging, nvidia-cublas-cu11, numpy, more-itertools, lxml, idna, future, fsspec, filelock, colorama, charset-normalizer, certifi, tqdm, requests, ffmpeg-python, cffi, huggingface-hub
+
+
 import os
 import re
 import json
 import shutil
+import emoji # New import, required to remove non standard chars 
 
 from PIL import Image
 from glob import glob
@@ -23,7 +31,7 @@ class Percentage(Perc):
         return '%.2f%%' % pbar.percentage()
 
 
-def unpacked() -> Tuple(str):
+def unpacked() -> "tuple(str)": #makes it work
     cwd = os.getcwd()
     archive_files = glob(cwd + '/' + '*.zip')
     whatsapp_archive = None
@@ -55,16 +63,17 @@ def convert_to_docx(file_name_txt: str, whatsapp_extract_dir: str, result_file_n
 
     with open(file_name_txt) as f:
         for i in f:
-            suffix = i.split()[-1][:-1].split('.')[-1]
-            if suffix in img_data:
-                document.add_paragraph(i)
-                filename = whatsapp_extract_dir + '/' + i.split()[-1][:-1]
-                convert_image = image_to_jpg(filename)
-                document.add_picture(convert_image, width=Inches(6))
-            else:
-                document.add_paragraph(i)
-
-        document.save(os.getcwd() + '/' + result_file_name + '.docx')
+            if not i=='\n' #doesnt like empty lines, so remove them.
+                suffix = i.split()[-1][:-1].split('.')[-1]
+                if suffix in img_data:
+                    document.add_paragraph(i)
+                    filename = whatsapp_extract_dir + '/' + i.split()[-1][:-1]
+                    convert_image = image_to_jpg(filename)
+                    document.add_picture(convert_image, width=Inches(6))
+                else:
+                    document.add_paragraph(i)
+        
+        document.save(result_file_name + '.docx') #file path doubled up. result_file_name contains os.cwp
 
     os.remove(file_name_txt)
     shutil.rmtree(whatsapp_extract_dir)
@@ -97,14 +106,14 @@ def voice_recognition(filename: str) -> str:
 
 def run(whatsapp_extract_dir: str) -> str:
     try:
-        with open(whatsapp_extract_dir+'/_chat.txt', 'r') as file:
+        with open(whatsapp_extract_dir+'/_chat.txt', 'r', errors="ignore") as file: #makes it work
             file_size = len(file.read())
 
     except FileNotFoundError as e:
         print('Error! File not found in "_chat.txt" !')
         return
 
-    with open(whatsapp_extract_dir+'/_chat.txt', 'r') as f, open('result.txt', 'w') as f2:
+     with open(replace_string(whatsapp_extract_dir+'/_chat.txt'), 'r', encoding='utf8', errors="ignore") as f, open('result.txt', 'w') as f2: #non standard characters cause issues, I brute force removed them
         step = 0
         bar = progressbar.ProgressBar(
             maxval=file_size,
@@ -134,12 +143,26 @@ def run(whatsapp_extract_dir: str) -> str:
                 filename = whatsapp_extract_dir + '/' + i.split()[-1][:-1]
                 text_rec = voice_recognition(filename)
                 i = i + '\t' + '-> ' + text_rec + '\n'
-            f2.write(i)
+            f2.write(replace_string(i)) #non standard characters cause issues, I brute force removed them
             bar.update(step)
         bar.finish()
         print('All files transcribe well done!')
         
         return 'result.txt'
+         
+def replace_string(text): #I'm a brute, add any characters from forign charsets here
+    text = emoji.replace_emoji(text,replace='')
+    text = text.replace(u"\u202a",'')
+    text = text.replace(u"\u202c",'')
+    text = text.replace(u"\u202f",'')
+    text = text.replace(u"\u200e",'')
+    text = text.replace(u"\u015e",'S')
+    text = text.replace(u"\u015f",'s')
+    text = text.replace(u"\u0308",'O')
+    text = text.replace(u"\u0327",'O')
+    text = text.replace(u"\u0130",'i')
+    text = text.replace(u"\u0131",'i')
+    return text
 
 
 def main() -> None:
